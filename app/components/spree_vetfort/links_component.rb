c05c04d3afd2b8d5db8a7dec292abbd523fb98ad
Vladimir Railean <raileanv@gmail.com>
2024-06-22 07:58:21 +0300

    fetch links from env + bump version

--- app/components/spree_vetfort/links_component.rb
+++ app/components/spree_vetfort/links_component.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 class SpreeVetfort::LinksComponent < ViewComponent::Base
+  def vetfort_link
+    ENV.fetch('VETFORT_LINK', 'https://vetfort.md')
+  end
 
+  def instagram_link
+    ENV.fetch('INSTAGRAM_LINK', 'https://www.instagram.com/zoo_magazine_vetfort.md/')
+  end
+
+  def tiktok_link
+    ENV.fetch('TIKTOK_LINK', 'https://www.tiktok.com/@vetfort_zoomagazin?_t=8mwF8K1PDZG')
+  end
 end
--- lib/spree_vetfort/version.rb
+++ lib/spree_vetfort/version.rb
@@ -1,5 +1,5 @@
 module SpreeVetfort
-  VERSION = '0.0.3'.freeze
+  VERSION = '0.0.4'.freeze
 
   module_function
 
